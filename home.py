bddfb7247eb4e0ad17e584582a34f723047795fa
oyvindsabo <oyvind.sabo@gmail.com>
2018-11-13 20:22:35 +0100

    Add basic routing

---
+++ home.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+# User profile
+@app.route('/user/<username>')
+def showUserProfile(username):
+    print(username)
+    return f'User {username}'
+
+# All forums
+@app.route('/forums/')
+def showForums():
+    return 'Forums'
+
+# Forum
+@app.route('/forums/<forumName>/')
+def showForum(forumName):
+    return f'Forum {forumName}'
+
+# Forum thread
+@app.route('/forums/<forumName>/<username>/')
+def showPost(forumName, username):
+    return f'Post {forumName}, {username}'
