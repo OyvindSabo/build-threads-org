54c99adfa9181c3b1249518ee09d329dce3fcdcc
oyvindsabo <oyvind.sabo@gmail.com>
2018-11-16 08:34:36 +0100

    Make basic block post component

--- views/libs/frontLib/blockPost.py
+++ views/libs/frontLib/blockPost.py
@@ -1,10 +1,12 @@
-def blockPost(author, post):
+def blockPost(post):
   return f"""
-    <div style='margin=5px'>
-      <a href='{author.profileLink}>{author.name}</a>
-      <img src='{post.imageUrl}'>
-      <h2>
-        <a href='{post.threadLink}'>{post.title}</a>
-      </h2>
-    </div>
+    <center>
+      <div class='blockPostContainer'>
+        <a href='{post['author']['profileLink']}' class='userName'>{post['author']['name']}</a>
+        <img src='{post['imageUrl']}' class='blockPostImage'>
+        <h2>
+          <a href='{post['postUrl']}' class='postTitle'>{post['title']}</a>
+        </h2>
+      </div>
+    </center>
   """
