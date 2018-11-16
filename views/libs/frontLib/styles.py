36f79a92dd7b01998c8bc972363d37f84bbc71ec
oyvindsabo <oyvind.sabo@gmail.com>
2018-11-16 08:33:37 +0100

    Create styles file for CSS

---
+++ views/libs/frontLib/styles.py
@@ -0,0 +1,25 @@
+def getStyles():
+  return """
+    <style>
+      .blockPostContainer {
+        box-shadow: 0px 0px 50px;
+        max-width:100vmin;
+        padding: 5px;
+        margin:5px;
+        width:100%;
+      }
+      .blockPostImage {
+        width: 100%;
+      }
+      .postTitle {
+        font-family: Arial Black;
+        text-decoration: none;
+        color: #2F3D45;
+      }
+      .userName {
+        font-family: Arial Black;
+        text-decoration: none;
+        color: #2F3D45;
+      }
+    </style>
+  """
